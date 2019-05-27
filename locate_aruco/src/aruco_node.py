bfe7dd28f43957a214950c58f0a8b97860753278
TrevorMuraro <trevormuraro@gmail.com>
2019-05-27 13:54:30 -0700

    Setting up ARUCO detection attempt (contingent on finding a ruler)

--- locate_aruco/src/aruco_node.py
+++ locate_aruco/src/aruco_node.py
@@ -11,6 +11,18 @@ from sensor_msgs.msg import Image
 # Launch file needs to launch usb_cam_node
 # Subscribe to /usb_cam/image_raw, which publishes sensor_msgs/Image
 
+ARUCO_PARAMETERS = aruco.DetectorParameters_create()
+ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_6X6_250)
+
+# The grid board type we're looking for
+board = arudo.GridBoard_create(
+        markersX = 1,
+        markersY = 1,
+        markerLength = # In meters
+        markerSeparation = 0, # In meters
+        dictionary = ARUCO_DICT
+    )
+
 class ArucoNode():
 
     def __init__(self):
@@ -22,3 +34,6 @@ class ArucoNode():
     def image_callback(self, img_msg):
 
         cv_img = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding="passthrough")
+
+
+        # aruco.estimatePoseSingleMarkers
