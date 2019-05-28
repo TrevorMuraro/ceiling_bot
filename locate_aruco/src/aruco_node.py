33ddda72f3541624c5eef03862b4739cda6d88aa
TrevorMuraro <trevormuraro@gmail.com>
2019-05-27 19:38:13 -0700

    Feeling out the (undocumented) Aruco library

--- locate_aruco/src/aruco_node.py
+++ locate_aruco/src/aruco_node.py
@@ -15,25 +15,52 @@ ARUCO_PARAMETERS = aruco.DetectorParameters_create()
 ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_6X6_250)
 
 # The grid board type we're looking for
-board = arudo.GridBoard_create(
+board = aruco.GridBoard_create(
         markersX = 1,
         markersY = 1,
-        markerLength = # In meters
-        markerSeparation = 0, # In meters
+        markerLength = 0.168, # In meters
+        markerSeparation = 0.1, # In meters
         dictionary = ARUCO_DICT
     )
+
+rvecs, tvecs = None, None
 
 class ArucoNode():
 
     def __init__(self):
 
+        #rospy.loginfo("Starting node")
+
         self.image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, callback=self.image_callback)
 
         self.bridge = cv_bridge.CvBridge()
 
+        rospy.spin()
+
     def image_callback(self, img_msg):
+
+        #rospy.loginfo("Received image")
 
         cv_img = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding="passthrough")
 
+        gray_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
+
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray_img, ARUCO_DICT, parameters=ARUCO_PARAMETERS)
+
+        rospy.loginfo("corners: {corners}, ids: {ids}, rejectedImgPoints: {rejects}".format(corners=corners, ids=ids, rejects=rejectedImgPoints))
+
+        """
+        if ids is not None and len(ids) == 5:
+            for i, corner in zip(ids, corners):
+                rospy.loginfo("id: {id}, corner: {corner}".format(id=i, corner=corner))
+        """
 
         # aruco.estimatePoseSingleMarkers
+
+if __name__ == '__main__':
+    rospy.init_node('aruco_node')
+
+    try:
+        n = ArucoNode()
+    except rospy.ROSInterruptException:
+        pass
