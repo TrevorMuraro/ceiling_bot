31e10f22d1ea0b8f92bdc62ba47f11fa8af5c01a
TrevorMuraro <trevormuraro@gmail.com>
2019-06-28 13:14:05 -0700

    Added camera calibration stuff

--- locate_aruco/src/aruco_node.py
+++ locate_aruco/src/aruco_node.py
@@ -4,12 +4,13 @@ import rospy
 import cv_bridge
 import cv2
 from cv2 import aruco
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 import numpy as np
 
 
 
 # Launch file needs to launch usb_cam_node: rosrun usb_cam usb_cam_node
+# To get around the error, first roslaunch usb_cam usb_cam-test.launch
 # Subscribe to /usb_cam/image_raw, which publishes sensor_msgs/Image
 
 ARUCO_PARAMETERS = aruco.DetectorParameters_create()
@@ -26,9 +27,7 @@ board = aruco.GridBoard_create(
 
 rvecs, tvecs = None, None
 
-CAMERA_MATRIX = 
 
-DISTORTION_COEFFICIENTS = 
 
 class ArucoNode():
 
@@ -37,40 +36,63 @@ class ArucoNode():
         #rospy.loginfo("Starting node")
 
         self.image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, callback=self.image_callback)
+        self.caminfo_sub = rospy.Subscriber('/usb_cam/camera_info', CameraInfo, callback=self.caminfo_callback)
         self.debug_image_pub_1 = rospy.Publisher('/locate_aruco/aruco_debug_stage_1', Image, queue_size=10)
         self.debug_image_pub_2 = rospy.Publisher('/locate_aruco/aruco_debug_stage_2', Image, queue_size=10)
 
         self.bridge = cv_bridge.CvBridge()
 
+        self.CAMERA_MATRIX = None
+        self.DISTORTION_COEFFICIENTS = None
+        self.have_cam_info = False
+
         rospy.spin()
+
+    def caminfo_callback(self, caminfo_msg):
+
+        if self.have_cam_info:
+            pass
+        else:
+            if caminfo_msg.K == [0, 0, 0, 0, 0, 0, 0, 0, 0]:
+                rospy.logwarn("CameraInfo message has K matrix all zeros")
+            else:
+                self.DISTORTION_COEFFICIENTS = caminfo_msg.D
+                self.CAMERA_MATRIX = [[0] * 3] * 3
+
+                for i in range(0, 3):
+                    for j in range(0, 3):
+                        self.CAMERA_MATRIX[i][j] = caminfo_msg.K[i * 3 + j]
+
+                self.have_cam_info = True
+
 
     def image_callback(self, img_msg):
 
-        #rospy.loginfo("Received image")
+        if self.have_cam_info:
 
-        cv_img = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding="passthrough")
+            cv_img = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding="passthrough")
 
-        gray_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
+            gray_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
 
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray_img, ARUCO_DICT, parameters=ARUCO_PARAMETERS)
+            corners, ids, rejectedImgPoints = aruco.detectMarkers(gray_img, ARUCO_DICT, parameters=ARUCO_PARAMETERS)
 
-        """
-        test_img = aruco.drawDetectedMarkers(cv_img, corners, borderColor=(0, 0, 255))
-        rosified_test_img = self.bridge.cv2_to_imgmsg(test_img, encoding="rgb8")
+            """
+            test_img = aruco.drawDetectedMarkers(cv_img, corners, borderColor=(0, 0, 255))
+            rosified_test_img = self.bridge.cv2_to_imgmsg(test_img, encoding="rgb8")
 
-        self.debug_image_pub_1.publish(rosified_test_img)
-        """
+            self.debug_image_pub_1.publish(rosified_test_img)
+            """
 
-        rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(corners, 0.168, CAMERA_MATRIX, DISTORTION_COEFFICIENTS)
+            rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(corners, 0.168, CAMERA_MATRIX, DISTORTION_COEFFICIENTS)
 
 
-        rospy.loginfo("rvecs: {rvecs}, tvecs: {tvecs}, _objPoints: {points}".format(rvecs=rvecs, tvecs=tvecs, points=_objPoints))
+            rospy.loginfo("rvecs: {rvecs}, tvecs: {tvecs}, _objPoints: {points}".format(rvecs=rvecs, tvecs=tvecs, points=_objPoints))
 
-        if rvecs is not None:
-            test_img_2 = aruco.drawAxis(cv_img, CAMERA_MATRIX, DISTORTION_COEFFICIENTS, rvecs[0], tvecs[0], 0.168)
-            rosified_test_img_2 = self.bridge.cv2_to_imgmsg(test_img_2, encoding="rgb8")
+            if rvecs is not None:
+                test_img_2 = aruco.drawAxis(cv_img, CAMERA_MATRIX, DISTORTION_COEFFICIENTS, rvecs[0], tvecs[0], 0.168)
+                rosified_test_img_2 = self.bridge.cv2_to_imgmsg(test_img_2, encoding="rgb8")
 
-            self.debug_image_pub_2.publish(rosified_test_img_2)
+                self.debug_image_pub_2.publish(rosified_test_img_2)
 
 
 
